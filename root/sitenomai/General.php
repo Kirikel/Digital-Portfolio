ea3a4e842737c644e9839674faad134d44fc03e7
Hayarov <126082149+Hayarov@users.noreply.github.com>
2023-05-18 18:50:09 +0500

    General.php

--- root/sitenomai/General.php
+++ root/sitenomai/General.php
@@ -60,6 +60,21 @@
 	  ?>
 	</div>
     <body>
+
+	<div class="container-menu">
+	</div>	
+	<div class="hamburger-menu">
+	  <input id="menu__toggle" type="checkbox" />
+	  <label class="menu__btn" for="menu__toggle">
+		<span></span>
+	  </label>
+	  <ul class="menu__box">
+		<li><a class="menu__item" href="General.php">Главная</a></li>
+		<li><a class="menu__item" href="Site.php">Регистрация </a></li>
+		<li><a class="menu__item" href="auth.php">Авторизация</a></li>
+		<li><a class="menu__item" href="dashboard.php">Профиль</a></li>
+	  </ul>
+	</div>	    
     <div>
         <?php
 			// Установка соединения с базой данных
