3378c470dc5650e6bf615802be8c4d6e6ec9e5f2
Hayarov <126082149+Hayarov@users.noreply.github.com>
2023-05-26 23:05:53 +0500

    Update DopInformationCheck.php

--- root/sitenomai/DopInformationCheck.php
+++ root/sitenomai/DopInformationCheck.php
@@ -1,24 +1,24 @@
-
 <head>
 	<link rel="stylesheet" href="style.css">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <html>
-	<div class="topnav">
-      <a class="active" href="general.php">Дом</a>
-      <?php 
-	  session_start();
-	  if($_SESSION['login']==null) 
-	  {
-		  echo "<a class='news' href='Site.php'> Регистрация </a><a class='news' href='auth.html'> Авторизация</a>";
-	  }
-	  else 
-	  {
-		  echo "<a class='posteto' href='post.php'>Создание статьи</a><a class='news' href='dashboard.php'>Профиль</a>";
-	  }
-	  ?>
+	<div class="container-menu">
+	</div>	
+	<div class="hamburger-menu">
+	  <input id="menu__toggle" type="checkbox" />
+	  <label class="menu__btn" for="menu__toggle">
+		<span></span>
+	  </label>
+	  <ul class="menu__box">
+		<li><a class="menu__item" href="General.php">Главная</a></li>
+		<li><a class="menu__item" href="Site.php">Регистрация </a></li>
+		<li><a class="menu__item" href="auth.php">Авторизация</a></li>
+		<li><a class="menu__item" href="dashboard.php">Профиль</a></li>
+	  </ul>
 	</div>
 
+<html>
 
 	<head>
 		<style>
