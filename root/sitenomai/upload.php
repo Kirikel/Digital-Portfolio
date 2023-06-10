4dfa88c6c0ea0519b2fa05daa862d47acf731e13
Hayarov <126082149+Hayarov@users.noreply.github.com>
2023-06-10 18:43:43 +0500

    upload.php

--- root/sitenomai/upload.php
+++ root/sitenomai/upload.php
@@ -1,3 +1,21 @@
+<html>
+	<body>
+
+	<div class="container-menu">
+	</div>	
+	<div class="hamburger-menu">
+	  <input id="menu__toggle" type="checkbox" />
+	  <label class="menu__btn" for="menu__toggle">
+		<span></span>
+	  </label>
+	  <ul class="menu__box">
+		<li><a class="menu__item" href="General.php">Главная</a></li>
+		<li><a class="menu__item" href="Site.php">Регистрация </a></li>
+		<li><a class="menu__item" href="auth.php">Авторизация</a></li>
+		<li><a class="menu__item" href="dashboard.php">Профиль</a></li>
+	  </ul>
+	</div>	    
+    <div>
 <?php
 session_start();
 if(isset($_FILES['image'])){
@@ -30,4 +48,4 @@ if(isset($_FILES['image'])){
     }
 }
 ?>
-
+</html>
