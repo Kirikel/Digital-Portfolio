24d40e1dff6e2774c31d8117e6bd51adbc2b6630
Hayarov <126082149+Hayarov@users.noreply.github.com>
2023-06-18 17:07:16 +0500

    Update auth.php

--- root/sitenomai/auth.php
+++ root/sitenomai/auth.php
@@ -8,26 +8,28 @@
 
 ?>
 <head>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="style12.css">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 </head>
 <html>
-	<div class="container-menu">
+	<div style="font-size: 50px;" class="container-menu">
+		MyCase
 	</div>	
 	<div class="hamburger-menu">
-	  <input id="menu__toggle" type="checkbox" />
-	  <label class="menu__btn" for="menu__toggle">
-		<span></span>
-	  </label>
-	  <ul class="menu__box">
-		<li><a class="menu__item" href="General.php">Главная</a></li>
-		<li><a class="menu__item" href="Site.php">Регистрация </a></li>
-		<li><a class="menu__item" href="auth.php">Авторизация</a></li>
-		<li><a class="menu__item" href="dashboard.php">Профиль</a></li>
-	  </ul>
+    <input id="menu__toggle" type="checkbox" />
+    <label class="menu__btn" for="menu__toggle">
+      <span></span>
+    </label>
+
+    <ul class="menu__box">
+      <li><a class="menu__item" href="General.php">Главная</a></li>
+      <li><a class="menu__item" href="Site.php">Регистрация</a></li>
+      <li><a class="menu__item" href="auth1.php">Авторизация</a></li>
+      <li><a class="menu__item" href="dashboard.php">Профиль</a></li>
+      <li><a class="menu__item" href="post.php">Создание постов</a></li>
+	  <li><a class="menu__item" href="DopInformationCheck.php">Обо мне</a></li>
+    </ul>
 	</div>
-
-
 
 <html>
 
@@ -40,9 +42,9 @@
 				position: absolute;
 				width: 500px;
 				height: 650px;
-				left: 770px;
+				left: 700px;
 				top: 250px;
-
+				position: center;
 				background: #101010;
 				box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 				border-radius: 30px;
@@ -66,14 +68,16 @@
 				display: flex;
 				align-items: center;
 				text-align: center;
-				color: white;
+
+				color: #FFFFFF;
 
 			}
+			
 			.login
 			{
 
 				box-sizing: border-box;
-
+				color: white;
 				position: absolute;
 				width: 388px;
 				height: 44px;
@@ -82,10 +86,9 @@
 				background: #232323;
 				border: 1px solid #FFFFFF;
 				border-radius: 10px;
-				color: white;
-			}
 
-			.password
+			}
+			.name
 			{
 				box-sizing: border-box;
 				position: absolute;
@@ -93,6 +96,19 @@
 				height: 44px;
 				left: 56px;
 				top: 300px;
+				color: white;
+				background: #232323;
+				border: 1px solid #FFFFFF;
+				border-radius: 10px;
+			}
+			.password
+			{
+				box-sizing: border-box;
+				position: absolute;
+				width: 388px;
+				height: 44px;
+				left: 56px;
+				top: 364px;
 				color: white;
 				background: #232323;
 				border: 1px solid #FFFFFF;
@@ -119,6 +135,7 @@
 					<input class="password" type="password" name="pass" class="form-control" id="pass" placeholder="Пароль" required><br>
 					<button class="btn btn-success">Войти</button><br>
 				</form> 
+				</div>
 		</div>
 		
 	</body>
