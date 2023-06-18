4fb669b3f1bf398ca069b0eeedeaf6daab6bc765
Hayarov <126082149+Hayarov@users.noreply.github.com>
2023-06-18 17:04:36 +0500

    Update General.php

--- root/sitenomai/General.php
+++ root/sitenomai/General.php
@@ -1,6 +1,5 @@
-
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style12.css">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<style>
 		textarea
@@ -15,8 +14,8 @@
 		}
 		.profile a
 		{
-			background-color: black;
-			color: #f2f2f2;
+			background-color: white;
+			color: black;
 			text-align: center;
 			text-decoration: none;
 			font-size:20px;
@@ -27,43 +26,76 @@
 			
 		.profile a:hover 
 		{
-			background-color: #ddd;
-			color: black;
-			float: right;
+			background-color: black;
+			color: white;
+
 			margin-top: 20px;
 			margin-bottom: 20px;
 		}
-
 		.profile a.active
 		{
-			float: left;
-			background-color: #ddd;
-			color: black;
+
+			background-color: black;
+			color: white;
 			margin-top: 20px;
 			margin-bottom: 20px;
 		}
 	</style>
+	  <style>
+	table {
+  border-collapse: collapse;
+  width: 100%;
+}
+thead {
+  font-weight: bold;
+}
+th,
+td {
+  padding: 0.5rem;
+  text-align: left;
+  border: 1px solid black;
+}
+	.container
+	{
+		margin-top: 15%;
+	}
+	.dopinfo
+	{
+		background-color: black;
+		color: white;
+	}
+	.container
+	{
+		text-align: center;
+	}
+	#container { text-align: center width: 25%; margin:0 auto; margin-top: 5%; background-color: black; color: white; border-radius: 5%;}
+  </style>
 </head>
 <html>
-
-    <body>
-
-	<div class="container-menu">
+	<div style="font-size: 50px;" class="container-menu">
+		MyCase
 	</div>	
 	<div class="hamburger-menu">
-	  <input id="menu__toggle" type="checkbox" />
-	  <label class="menu__btn" for="menu__toggle">
-		<span></span>
-	  </label>
-	  <ul class="menu__box">
-		<li><a class="menu__item" href="General.php">Главная</a></li>
-		<li><a class="menu__item" href="Site.php">Регистрация </a></li>
-		<li><a class="menu__item" href="auth.php">Авторизация</a></li>
-		<li><a class="menu__item" href="dashboard.php">Профиль</a></li>
-	  </ul>
-	</div>	    
-    <div>
-        <?php
+    <input id="menu__toggle" type="checkbox" />
+    <label class="menu__btn" for="menu__toggle">
+      <span></span>
+    </label>
+
+    <ul class="menu__box">
+      <li><a class="menu__item" href="General.php">Главная</a></li>
+      <li><a class="menu__item" href="Site.php">Регистрация</a></li>
+      <li><a class="menu__item" href="auth1.php">Авторизация</a></li>
+      <li><a class="menu__item" href="dashboard.php">Профиль</a></li>
+      <li><a class="menu__item" href="post.php">Создание постов</a></li>
+	  <li><a class="menu__item" href="DopInformationCheck.php">Обо мне</a></li>
+    </ul>
+	</div>
+
+	<body>
+	<div class='container'>
+	
+	
+	<?php
 			// Установка соединения с базой данных
 			$host = 'localhost';
 			$user = 'root';
@@ -80,31 +112,46 @@
 			// Отображение статей
 			if (mysqli_num_rows($result) > 0) 
 			{
+				echo "<tbody>";
 				while ($row = mysqli_fetch_assoc($result)) 
 				{
-					
-					echo "<h3>Фамилия: {$row['login']}</h3>";
-					echo "<h3>Имя: {$row['name']}</h3>";
-					echo "<h3>Обо мне:</h3>";
+					echo '<div style="width: 25%; border-radius: 2" id="container">';
+					echo "<tr>";
 					$host = "localhost";
 					$username = "root";
 					$password = "usbw";
 					$dbname = "register-bd";
 					$connection = mysqli_connect($host, $username, $password, $dbname);
 
+					
+					$id=$row['id'];	
+					$query = "SELECT * FROM user_img WHERE id_user='$id'";
+					$result1 = mysqli_query($connection, $query);
+					$row2 = mysqli_fetch_assoc($result1);
+					if ($row2!="")
+					{
+						echo '<td><img width="200" height="200" src="'.$row2["imageurl"].'" alt="My Image"></td>';
+					}
+					echo "<td><h3>Фамилия: {$row['login']}</h3></td>";
+					echo "<td><h3>Имя: {$row['name']}</h3></td>";
+					echo "<td><h3>Обо мне:</h3></td>";
+					
 
-					$id=$row['id'];
+					
 					  // Запрос к базе данных
 					  // Выводим данные из запроса
 					  $query1="SELECT * FROM dopinfo WHERE id_user='$id'";
 					  $result1=mysqli_query($connection, $query1);	
 					  $row1=mysqli_fetch_assoc($result1);
-					  echo "<td><textarea readonly cols='125' rows='15' resize='none'>". $row1["dopinfo"]."</textarea></td>";
+					  echo "<td><textarea class='dopinfo' readonly cols='50' rows='15' resize='none'>". $row1["dopinfo"]."</textarea></td>";
 					  echo "<h1></h1>";
-					  echo '<div class=profile><a class=news href="article.php?id=' . $row['id'] . '">Перейти</a><br>';
+					  echo '<div class=profile><td><a class=news href="article.php?id=' . $row['id'] . '">Перейти</a><br></td></div>';
 					  // Закрываем соединение с базой данных
+
+					 echo "</div>";
 					  mysqli_close($connection); 
 				}
+				
 			} 
 			else 
 			{
@@ -112,6 +159,6 @@
 			}
 			mysqli_close($conn);
 		?>
-    <div>
+    </div>
   </body>
 </html>
